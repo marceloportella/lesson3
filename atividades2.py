81993153a521122194275ac2639628d3319ffbe7
Marcelo Portella <marcelo.portella@outlook.com>
2025-01-12 05:20:21 -0300

    add a resolucao do exercicio 5

--- atividades2.py
+++ atividades2.py
@@ -56,3 +56,10 @@
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar transações suspeitas. 
 # Uma transação é considerada suspeita se o valor for superior a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como transacao = {'valor': 12000, 'hora': 20}, verifique se ela é suspeita.
+
+transacao = {'valor': 12000, 'hora': 20}
+
+if transacao['valor'] > 10000 or 9 < transacao['hora'] > 18:
+    print("transacao suspeita")
+else:
+    print("transacao ok")
